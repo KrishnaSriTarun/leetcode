17c4a5edfc1515e4acd41947c2b403005e676344
Krishna Sri Tarun <100195889+KrishnaSriTarun@users.noreply.github.com>
2024-11-20 22:00:03 +0530

    Time: 0 ms (100%), Space: 41.7 MB (19.46%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.java
+++ 0953-reverse-only-letters/0953-reverse-only-letters.java
@@ -1,23 +1,23 @@
 class Solution {
     public String reverseOnlyLetters(String s) {
-        StringBuilder rev=new StringBuilder("");
-        StringBuilder res=new StringBuilder("");
-        int j=0;
-        for(int i=0;i<s.length();i++){
-            if(Character.isLetter(s.charAt(i))){
-                rev.append(s.charAt(i));
+        int left=0;
+        int right=s.length()-1;
+        char[] ch=s.toCharArray();
+        while(left<right){
+            while(left<right && !Character.isLetter(ch[left])){
+                left++;
+            }
+            while(left<right && !Character.isLetter(ch[right])){
+                right--;
+            }
+            if(left<right){
+                char temp=ch[left];
+                ch[left]=ch[right];
+                ch[right]=temp;
+                left++;
+                right--;
             }
         }
-        rev.reverse();
-        for(int i=0;i<s.length();i++){
-            if(!Character.isLetter(s.charAt(i))){
-                res.append(s.charAt(i));
-            }
-            else{
-                res.append(rev.charAt(j));
-                j++;
-            }
-        }
-        return res.toString();
+        return new String(ch);
     }
 }
