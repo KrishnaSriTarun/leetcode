98d8bc865f1ec571217ef48a160616cc9d820ff5
Krishna Sri Tarun <100195889+KrishnaSriTarun@users.noreply.github.com>
2025-06-03 06:50:24 +0530

    Time: 12 ms (97.56%), Space: 46.5 MB (86.92%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,9 +1,13 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
+        int sum = 0;
         Arrays.sort(nums);
-        int sum=0;
-        for(int i=0;i<nums.length-1;i+=2){
-            sum+=Math.min(nums[i],nums[i+1]);
+
+        int i=0;
+        while (i<nums.length){
+            
+            sum += nums[i];
+            i+=2;
         }
         return sum;
     }
