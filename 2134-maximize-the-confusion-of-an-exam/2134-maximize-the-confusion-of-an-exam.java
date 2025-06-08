213923cce0f7ed7811b05feba07c1b22ba84a62a
Krishna Sri Tarun <100195889+KrishnaSriTarun@users.noreply.github.com>
2025-06-08 19:39:20 +0530

    Time: 12 ms (77.56%), Space: 45 MB (31.42%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.java
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.java
@@ -1,22 +1,51 @@
+// class Solution {
+//     public int maxConsecutiveAnswers(String arr, int k) {
+//         return Math.max(maxValues(arr,k,'T'),maxValues(arr,k,'F'));
+//     }
+//     private static int maxValues(String key, int k,char tar){
+//         int val=0;
+//         int i=0;
+//         int ans=0;
+//         for(int j=0;j<key.length();j++){
+//             if(key.charAt(j)==tar){
+//                 val++;
+//             }
+//             while(val>k){
+//                 if(key.charAt(i++)==tar){
+//                     val--;
+//                 }
+//             }
+//             ans=Math.max(ans,j-i+1);
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
-    public int maxConsecutiveAnswers(String arr, int k) {
-        return Math.max(maxValues(arr,k,'T'),maxValues(arr,k,'F'));
-    }
-    private static int maxValues(String key, int k,char tar){
-        int val=0;
-        int i=0;
-        int ans=0;
-        for(int j=0;j<key.length();j++){
-            if(key.charAt(j)==tar){
-                val++;
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int ans = 0;
+        int   ctnt=0;
+        int ctnf =0;
+        int l=0;
+        for(int r=0;r<answerKey.length();r++){
+            if (answerKey.charAt(r) == 'T'){
+                ctnt++;
             }
-            while(val>k){
-                if(key.charAt(i++)==tar){
-                    val--;
+            else{
+                ctnf++;
+            }
+            while(Math.min(ctnt,ctnf) > k){
+                if (answerKey.charAt(l) == 'T'){
+                    ctnt--;
                 }
+                else{
+                    ctnf--;
+                }
+                l++;
             }
-            ans=Math.max(ans,j-i+1);
+            ans = Math.max(ans,r-l+1);
         }
+
         return ans;
     }
 }
