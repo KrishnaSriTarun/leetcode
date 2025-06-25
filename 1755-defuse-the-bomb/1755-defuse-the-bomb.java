b8b7be309aa2fc1c920dee3f368e951ba92ba624
Krishna Sri Tarun <100195889+KrishnaSriTarun@users.noreply.github.com>
2025-06-25 13:14:06 +0530

    Time: 2 ms (60.32%), Space: 42.5 MB (24.72%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,37 +1,21 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int[] arr=new int[code.length];
+        int n=code.length;
+        int[] arr=new int[n];
         int val=Math.abs(k);
         int j=0;
-        for(int i=0;i<code.length;i++){
+        if(k==0) return arr;
+        for(int i=0;i<n;i++){
             int temp=0;
             int count=0;
-            if(k>0){
-                j=i+1;
-            }
-            else if(k<0){
-                j=i-1;
-            }
-            else{
-                j=0;
-            }
             while(count<val){
-                if(j>=code.length){
-                    j=0;
-                }
-                else if( j<0){
-                    j=code.length-1;
-                }
                 if(k>0){
-                    temp+=code[j++];
-                }
-                else if(k<0){
-                    temp+=code[j--];
+                    j=(i+count+1)%n;
                 }
                 else{
-                    temp=0;
+                    j=(i-count-1+n)%n;
                 }
-                
+                temp+=code[j];
                 count++;
             }
             arr[i]=temp;
