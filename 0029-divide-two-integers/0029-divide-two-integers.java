e58f11e9c6afe60e822355c818165922a88c6c0b
Krishna Sri Tarun <100195889+KrishnaSriTarun@users.noreply.github.com>
2025-06-26 12:10:56 +0530

    Time: 0 ms (100%), Space: 40.9 MB (60.17%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,8 +1,5 @@
 class Solution {
     public int divide(int d, int div) {
-        if (div == 0) {
-            throw new ArithmeticException("Division by zero");
-        }
         if (d == Integer.MIN_VALUE && div == -1) {
             return Integer.MAX_VALUE;
         }
