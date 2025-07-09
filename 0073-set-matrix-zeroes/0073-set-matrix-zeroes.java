f8e2febd922b2fff439c8c77fafec7698417b11d
Krishna Sri Tarun <100195889+KrishnaSriTarun@users.noreply.github.com>
2025-07-09 21:28:36 +0530

    Time: 0 ms (100%), Space: 45.7 MB (63.95%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,30 +1,24 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int col0=1;
-        for(int i=0;i<matrix.length;i++){
-            if (matrix[i][0] == 0) col0 = 0;
-            for(int j=1;j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    matrix[0][j]=0;
-                    matrix[i][0]=0; 
+        boolean zeroinFirstCol = false;
+        for (int row = 0; row < matrix.length; row++) {
+            if (matrix[row][0] == 0) zeroinFirstCol = true;
+            for (int col = 1; col < matrix[0].length; col++) {
+                if (matrix[row][col] == 0) {
+                    matrix[row][0] = 0;
+                    matrix[0][col] = 0;
                 }
             }
         }
-        for(int i=1;i<matrix.length;i++){
-            for(int j=1;j<matrix[0].length;j++){
-                if(matrix[0][j]==0 || matrix[i][0]==0){
-                    matrix[i][j]=0;
+
+        for (int row = matrix.length - 1; row >= 0; row--) {
+            for (int col = matrix[0].length - 1; col >= 1; col--) {
+                if (matrix[row][0] == 0 || matrix[0][col] == 0) {
+                    matrix[row][col] = 0;
                 }
             }
-        }
-        if(matrix[0][0]==0){
-            for(int j=0;j<matrix[0].length;j++){
-                matrix[0][j]=0;
-            }
-        }
-        if(col0==0){
-            for(int i=0;i<matrix.length;i++){
-                matrix[i][0]=0;
+            if (zeroinFirstCol) {
+                matrix[row][0] = 0;
             }
         }
     }
