77fb4673a521be564665190e2d7bea7d9f8fe959
Krishna Sri Tarun <100195889+KrishnaSriTarun@users.noreply.github.com>
2025-07-15 08:16:38 +0530

    Time: 1 ms (100%), Space: 45.4 MB (79.96%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -1,29 +1,29 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int n=grid.length;
-        int arr[]=new int[n*n];
-        int ans[]=new int[2];
-        int k=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                arr[k++]=grid[i][j];
+        int n = grid.length;
+        int N = n * n;
+        
+        long sn = (long) N * (N + 1) / 2;
+        long s2n = (long) N * (N + 1) * (2L * N + 1) / 6;
+        
+        long s = 0;
+        long s2 = 0;
+        long x = 0;
+        long y = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                s += grid[i][j];
+                s2 += (long) grid[i][j] * grid[i][j];
             }
         }
-        Arrays.sort(arr);
-        if(arr[0]!=1){
-            ans[1]=1;
-        }
-        if(arr[arr.length-1]!=n*n){
-            ans[1]=n*n;
-        }
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]==arr[i+1]){
-                ans[0]=arr[i];
-            }
-            if (arr[i + 1] - arr[i] == 2 && ans[1] == 0) {
-                ans[1] = arr[i] + 1; 
-            }
-        }
-        return ans;
+
+        long a = s - sn;
+        long b = s2 - s2n;
+        long c = b / a;
+        x = (c + a) / 2;
+        y = c - x;
+
+        return new int[]{(int) x, (int) y};
     }
 }
