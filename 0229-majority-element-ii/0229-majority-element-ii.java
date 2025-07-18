776e80f4c8348eab3ac3fb81f3da3c5c82025d19
Krishna Sri Tarun <100195889+KrishnaSriTarun@users.noreply.github.com>
2025-07-18 08:20:32 +0530

    Time: 4 ms (60.59%), Space: 50.6 MB (16.83%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,49 +1,39 @@
-// class Solution {
-//     public List<Integer> majorityElement(int[] nums) {
-//         HashMap<Integer,Integer> map=new HashMap<>();
-//         ArrayList<Integer> lis=new ArrayList<>();
-//         for(int num:nums){
-//             map.put(num, map.getOrDefault(num, 0) + 1);
-//         }
-//         for(Integer key: map.keySet()){
-//             if(map.get(key) > nums.length/3){
-//                 lis.add(key);
-//             }
-//         }
-//         return lis;
-//     }
-// }
-
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int count1 = 0, count2 = 0;
-        Integer candidate1 = null, candidate2 = null;
+        List<Integer> lis = new ArrayList<>();
+        Integer ele1 = null;
+        Integer ele2 = null;
+        int count1 = 0;
+        int count2 = 0;
 
         for (int num : nums) {
-            if (candidate1 != null && num == candidate1) {
+            if (ele1 != null && num == ele1) {
                 count1++;
-            } else if (candidate2 != null && num == candidate2) {
+            } else if (ele2 != null && num == ele2) {
                 count2++;
             } else if (count1 == 0) {
-                candidate1 = num;
+                ele1 = num;
                 count1 = 1;
             } else if (count2 == 0) {
-                candidate2 = num;
+                ele2 = num;
                 count2 = 1;
             } else {
                 count1--;
                 count2--;
             }
         }
-        count1 = count2 = 0;
+
+        // Recount to confirm majority
+        count1 = 0;
+        count2 = 0;
         for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+            if (ele1 != null && num == ele1) count1++;
+            else if (ele2 != null && num == ele2) count2++;
         }
 
-        List<Integer> result = new ArrayList<>();
-        if (count1 > nums.length / 3) result.add(candidate1);
-        if (count2 > nums.length / 3) result.add(candidate2);
-        return result;
+        if (count1 > nums.length / 3) lis.add(ele1);
+        if (count2 > nums.length / 3) lis.add(ele2);
+
+        return lis;
     }
 }
