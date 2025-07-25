e5149c2a1175d0eb8144b34860abf922ce3a6231
Krishna Sri Tarun <100195889+KrishnaSriTarun@users.noreply.github.com>
2025-07-25 22:29:00 +0530

    Time: 1 ms (91.25%), Space: 44.9 MB (74.47%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,31 +13,55 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> arr=new ArrayList<>();
+//         if(root==null) return arr;
+//         List<Integer> list=new ArrayList<>();
+//         Queue<TreeNode> q=new LinkedList<>();
+//         q.add(root);
+//         q.add(null);
+//         while(!q.isEmpty()){
+//             TreeNode curr=q.remove();
+//             if(curr==null){
+//                 arr.add(new ArrayList<>(list));
+//                 list.clear();
+
+//                 if(q.isEmpty()) break;
+//                 else q.add(null);
+//             }
+//             else{
+//                 list.add(curr.val);
+//                 if(curr.left!=null){
+//                     q.add(curr.left);
+//                 }
+//                 if(curr.right !=null){
+//                     q.add(curr.right);
+//                 }
+//             }
+//         }
+//         return arr;
+//     }
+// }
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> arr=new ArrayList<>();
         if(root==null) return arr;
-        List<Integer> list=new ArrayList<>();
+
         Queue<TreeNode> q=new LinkedList<>();
         q.add(root);
-        q.add(null);
         while(!q.isEmpty()){
-            TreeNode curr=q.remove();
-            if(curr==null){
-                arr.add(new ArrayList<>(list));
-                list.clear();
-                if(q.isEmpty()) break;
-                else q.add(null);
-            }
-            else{
+            List<Integer> list=new ArrayList<>();
+            int level=q.size();
+
+            for(int i=0;i<level;i++){
+                TreeNode curr=q.poll();
                 list.add(curr.val);
-                if(curr.left!=null){
-                    q.add(curr.left);
-                }
-                if(curr.right !=null){
-                    q.add(curr.right);
-                }
+                if(curr.left !=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
             }
+            arr.add(list);
+
         }
         return arr;
     }
